574a7540ae107573b1cf996c376026534230bfc1
ImRez69 <CaptianAlirez@gmail.com>
2025-08-06 01:01:24 +0330

    Add Part 5

--- Tutorial.py
+++ Tutorial.py
@@ -1,3 +1,31 @@
-# ----- Part 5 (  Data Types ) -----
+# --- Part 5 (  Data Types ) ------------------------------------------
+
+# Text Type : str
+# Numeric Types: int, float, complex
+
+# ---
+
+# print("Alireza") # String # str
+# print(type("alireza")) # Get Type
+
+# print("-----------------------------")
+
+# print(20) # Integer # int
+# print(type(20)) # Get Type
+# print(-20) # Integer # int
+# print(type(-20)) # Get Type
+
+# print("-----------------------------")
+
+# print(20.2) # Float # float
+# print(type(20.2)) # Get Type
+
+# print("-----------------------------")
+
+# print(20j) # Complex # complex
+# print(type(20j)) # Get Type
+
+# --- Part 6 ( Operators ) ------------------------------------------
 
 
+# ---
